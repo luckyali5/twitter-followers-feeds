4916415d3ccbbc771836467452a640aaa87f572a
luckyali5 <lucky_ali0926@yahoo.com>
2014-05-15 04:45:31 -0700

    rate limit

--- config.php
+++ config.php
@@ -4,6 +4,8 @@ define("CONSUMERKEY", "PBYpBd094YOy4x94BHCvPUEif");
 define("CONSUMERSECRET", "aJHcfwOY86YgeKO3vy0WBPI4nNHLM65AfsY6S7MSYhQZK8NRIq");
 define("OUTHTOKEN", "285444470-u23tmkZIfpNXuyiirSttNRRIz7KEYbGSUZUulb9i");
 define("OUTHSECRET", "EPAyLaYRgFCv99jZJWzVWRGm8OGzo1nuZk239MabYjkrr");
+
+// limit is set to 5 in order to avoide exceeding rate limit, you may change it to 50
 define("FEEDSLIMIT", 5);
 
  
