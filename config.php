33b82f380c3b29f39f589c45d7ff31f20bc98d06
luckyali5 <lucky_ali0926@yahoo.com>
2014-05-15 04:40:37 -0700

    Update config.php

--- config.php
+++ config.php
@@ -4,7 +4,7 @@ define("CONSUMERKEY", "uQGLEltdTnqVF6StIJ8dZkCIK");
 define("CONSUMERSECRET", "r1KqvBGNUSqvfipwGS6aQdgcyzLjVvelaL3QiZ0aDxJJvnuPSB");
 define("OUTHTOKEN", "285444470-aidqBg9jVTEmqkYVwEpidvz7V50Q8IVmpWqGvafG");
 define("OUTHSECRET", "kbTsppDgcVJ10cN2NYtwiL8V0r79GSq6e9RzjRRWtwZmy");
-define("FEEDSLIMIT", 50);
+define("FEEDSLIMIT", 5);
 
  
 
