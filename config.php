3bf62273a2c52ceecf7612ae996f2edc06e7087f
luckyali5 <lucky_ali0926@yahoo.com>
2014-05-15 04:43:55 -0700

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 
-define("CONSUMERKEY", "uQGLEltdTnqVF6StIJ8dZkCIK");
-define("CONSUMERSECRET", "r1KqvBGNUSqvfipwGS6aQdgcyzLjVvelaL3QiZ0aDxJJvnuPSB");
-define("OUTHTOKEN", "285444470-aidqBg9jVTEmqkYVwEpidvz7V50Q8IVmpWqGvafG");
-define("OUTHSECRET", "kbTsppDgcVJ10cN2NYtwiL8V0r79GSq6e9RzjRRWtwZmy");
+define("CONSUMERKEY", "PBYpBd094YOy4x94BHCvPUEif");
+define("CONSUMERSECRET", "aJHcfwOY86YgeKO3vy0WBPI4nNHLM65AfsY6S7MSYhQZK8NRIq");
+define("OUTHTOKEN", "285444470-u23tmkZIfpNXuyiirSttNRRIz7KEYbGSUZUulb9i");
+define("OUTHSECRET", "EPAyLaYRgFCv99jZJWzVWRGm8OGzo1nuZk239MabYjkrr");
 define("FEEDSLIMIT", 5);
 
  
